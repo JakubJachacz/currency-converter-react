9266093e8dc5d47d909fdf6d05e22c0a668ba36e
Jakub Jachacz <hexadoxpl@gmail.com>
2023-03-24 22:10:17 +0100

    added useCurrentDate hook

--- src/Calculate/Clock/index.js
+++ src/Calculate/Clock/index.js
@@ -1,29 +1,22 @@
-import { useState, useEffect } from "react";
-import "./style.css";
+import { useCurrentDate } from "./useCurrentDate";
+import { Wrapper } from "./styled";
+
+const formatDate = (date) => date.toLocaleString(undefined, {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "numeric",
+    month: "long"
+});
 
 export const Clock = () => {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalID);
-        };
-    }, []);
+    const date = useCurrentDate();
 
     return (
-        <div className="clock">
-            {date.toLocaleString(undefined, {
-                weekday: "long",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                day: "numeric",
-                month: "long"
-            })}
-        </div>
+        <Wrapper>
+            {" "}
+            {formatDate(date)}
+        </Wrapper>
     )
 };
