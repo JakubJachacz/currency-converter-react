f664c7f55e43c5b2979f4ed78816f97f1a810f82
Jakub Jachacz <hexadoxpl@gmail.com>
2023-03-29 03:38:36 +0200

    moved input above button

--- src/Calculate/Form/index.js
+++ src/Calculate/Form/index.js
@@ -32,61 +32,57 @@ export const Form = () => {
   }
 
   return (
-    <StyledForm onSubmit={onSubmit}>
+<StyledForm onSubmit={onSubmit}>
+  {ratesData.state === "loading" ? (
+    <Loading>
+      Prosimy poczekać, ładujemy kursy walut z Europejskiego Banku Centralnego
+    </Loading>
+  ) : ratesData.state === "error" ? (
+    <Fail>
+      Coś poszło nie tak. Sprawdź swoje połączenie z internetem lub spróbuj ponownie później.
+    </Fail>
+  ) : (
+    <>
       <fieldset>
         <legend>Kalkulator walut</legend>
-        {ratesData.state === "loading"
-          ? (
-            <Loading>
-              Prosimy poczekać, ładujemy kursy walut z Europejskiego Banku Centralnego
-            </Loading>
-          )
-          : (
-            ratesData.state === "error" ? (
-              <Fail>
-                Coś poszło nie tak. Sprawdź swoje połączenie z internetem lub spróbuj ponownie później.
-              </Fail>
-            ) : (
-              <>
-                <p>
-                  <label>
-                    <StyledSelect
-                      value={currency}
-                      onChange={({ target }) => setCurrency(target.value)}
-                    >
-                      {Object.keys(ratesData.rates).map(((currency) => (
-                        <option
-                          key={currency}
-                          value={currency}
-                        >
-                          {currency}
-                        </option>
-                      )))} 
-                    </StyledSelect>
-                  </label>
-                </p>
-                <p>
-                  <StyledButton type="submit">Przelicz</StyledButton>
-                </p>
-                <Result result={result} />
-              </>
-            )
-          )}
         <p>
           <label>
-            Kwota w PLN:
-            <StyledInput
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              placeholder="Wpisz kwotę w zł"
-              required
-              type="number"
-              min="0.01"
-              step="any"
-            />
+            <StyledSelect
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {Object.keys(ratesData.rates).map((currency) => {
+                return (
+                  <option key={currency} value={currency}>
+                    {currency}
+                  </option>
+                );
+              })}
+            </StyledSelect>
           </label>
+          <p>
+        <label>
+          Kwota w PLN:
+          <StyledInput
+            value={amount}
+            onChange={({ target }) => setAmount(target.value)}
+            placeholder="Wpisz kwotę w zł"
+            required
+            type="number"
+            min="0.01"
+            step="any"
+          />
+        </label>
+      </p>
+        </p>
+        <Result result={result} />
+        <p>
+          <StyledButton type="submit">Przelicz</StyledButton>
         </p>
       </fieldset>
-    </StyledForm>
-  );
-}
+    </>
+  )}
+</StyledForm>
+
+   );
+  }
